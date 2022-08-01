091de1ebbcbc3f8a508d7fc597f93a343e50d5ed
IT"Z DIPTO VAU OKAY <107912629+D1PTO-Official@users.noreply.github.com>
2022-08-01 23:21:19 +0600

    Create GF.py

---
+++ GF.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import random,os
+
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+os.system("clear")
+logo=str("""  \033[0;32m 8888888b. 8888888 8888888b. 88888888888 .d88888b.       
+\033[0;33m   888  "Y88b  888   888   Y88b    888    d88P" "Y88b      
+\033[0;32m   888    888  888   888    888    888    888     888      
+\033[0;33m   888    888  888   888   d88P    888    888     888      
+\033[0;32m   888    888  888   8888888P"     888    888     888      
+ \033[0;33m  888    888  888   888           888    888     888      
+\033[0;32m   888  .d88P  888   888           888    Y88b. .d88P      
+ \033[0;33m  8888888P" 8888888 888           888     "Y88888P"
+           \033[0;32m_____________________________________           
+        \033[0;32m  | AUTHOR    : MR. D1PTO\033[0m             \033[0;32m  |
+          |\033[0;35m GITHUB    : git.me/D1PTO-Official\033[0m \033[0;32m  |
+          |\033[0;36m FACEBOCP  : D1PTO.Fake420        \033[0m \033[0;32m  |
+          |\033[0;37m Group     : TUFAN FIGHTER GANG\033[0m   \033[0;32m   |
+          |\033[0;31m WHATSAPP  : [ ERROR ]\033[0m            \033[0;32m   | 
+           _____________________________________
+
+___________________________________________________________
+___________________________________________________________""")       
+print(logo)  
+us=str(input(green+"Enter Your Username :"))
+pas=str(input(purple+"Enter Your Password :"))
+
+fuck="A"
+sex="B"
+
+if us==fuck and pas==sex:
+	os.system("clear")
+	print(green+"[✓] Your Login Sucses-!!")
+else:
+	print(red+"[x] Wrong Password-!")
+	sys.exit() 
+def GF():
+    Gff = ('(×)\033[1;32m Tor Kopal Kharap Kono Din GF Pabi Nah😭\n\033[1;37m','\n(😁) \033[1;33mAba Tor Kopale To Onek GF Ace🐸\n\033[1;37m ','(👅)\033[1;31m Tui GF Pabi But Sob Barovatari  💔 \n\033[1;37m')
+    os.system('clear')
+    print(logo)
+    input('(√)\033[1;32mTomar Luck try Koro GF Paba Naki Paba Nah🙂💔\n\n(•)\033[1;37m Press Enter For Run : \n')
+    Gf = random.choice(Gff)
+    print('(√) Your Luck :\n\n '+Gf)
+    input('(√) \033[1;32m Press Enter To Try Again : ');GF()
+GF()
